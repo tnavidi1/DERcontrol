a6ed472b8c96f4d9e0bf524666307ba068ec2fa6
abidwael2 <38028370+abidwael2@users.noreply.github.com>
2018-07-18 13:12:49 -0700

    A small correction to sampling.py

--- sampling.py
+++ sampling.py
@@ -12,7 +12,7 @@ class Resampling:
 		self.t = t
 		self.tnew = tnew
 	def downsampling(self, data, t, tnew):
-		downsampled = [[]]
+		downsampled = []
 		t_ratio = tnew // t
 		node_num = 0
 		for node in data:
@@ -21,7 +21,7 @@ class Resampling:
 			for time in node:
 				if i >= t_ratio:
 					avg = time_sum / t_ratio
-					downsampled[node_num].append(avg)
+					downsampled[node_num].append([avg])
 					avg = 0
 					i = 0
 				else:
@@ -31,8 +31,8 @@ class Resampling:
 		return downsampled
 
 	def upsampling(self, data, t, tnew):
-		upsampled = [[]]
-		upsampled_final = [[]]
+		upsampled = []
+		upsampled_final = []
 		node_num = 0
 		min_per_day = 1440
 		t_ratio = t // tnew
@@ -44,14 +44,14 @@ class Resampling:
 				tot_at_time_of_day = 0
 				for j in range(days):
 					tot_at_time_of_day += node[i + j]
-				upsampled[node_num].append(tot_at_time_of_day / days)
+				upsampled[node_num].append([tot_at_time_of_day / days])
 			node_num += 1
 		node_num = 0
 		sigma = 0.1
 		for node in upsampled:
 			for time in node:
-				for i in range(t_ratio)
-				upsampled_final[node_num].append(np.random.normal(time, sigma))
+				for i in range(t_ratio):
+					upsampled_final[node_num].append([np.random.normal(time, sigma)])
 			node_num += 1
 		return upsampled_final
 
