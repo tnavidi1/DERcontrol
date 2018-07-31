bdeb20cc051988016737526dbe4c68ed968bcf2c
tnavidi1 <tnavidi@stanford.edu>
2018-07-30 19:10:30 -0700

    added comments for improvements

--- aggregate.py
+++ aggregate.py
@@ -1,19 +1,43 @@
+"""
+Do not use future division use numpy arrays
+data = np.array(data,dtype=float)
+"""
 from __future__ import division
+
 import numpy as np
 import random
 
 def daily_average(l, time_resolution):
+    """
+    we dont want to compare the daily average we want to compare the average daily maximum
+    To do this we would find the maximum value for each day
+    Then average together every day's maximum value
+
+    """
     s = sum(l)
     time_total = time_resolution * len(l)
     daily_average = 1440 * (s / time_total)
     return daily_average
 
 def aggregate(data, max_loads, daily_max = False, time_resolution = np.nan):
+    """
+    It is good practice to use the triple quotes and a few sentences about what the object or function does.
+    Inputs: data - description
+            etc
+
+    Outputs: new_data - description
+    """
     if daily_max == False:
         new_data = []
         adjustment_rate = 0
         for i in range(len(max_loads)):
+            """ If you want to initialize an array use np.zeros(size)
+            """
             temp = [0] * len(data[0])
+            """
+            not good practice to use while True and if break
+            Just put not if statement in the while loop
+            """
             while True:
                 num_row = random.randint(0, len(data) - 1)
                 for j in range(len(temp)):
@@ -25,13 +49,17 @@ def aggregate(data, max_loads, daily_max = False, time_resolution = np.nan):
             new_data.append(temp)
         return new_data
     else:
-        if time_resolution == np. nan:
+        if time_resolution == np.nan:
             return []
         else:
             new_data = []
             adjustment_rate = 0
             for i in range(len(max_loads)):
                 temp = [0] * len(data[0])
+                """
+                not good practice to use while True and if break
+                Just put not if statement in the while loop
+                """
                 while True:
                     num_row = random.randint(0, len(data) - 1)
                     for j in range(len(temp)):
@@ -41,6 +69,11 @@ def aggregate(data, max_loads, daily_max = False, time_resolution = np.nan):
                         break
                 temp[:] = [x // adjustment_rate for x in temp]
                 new_data.append(temp)
+                """
+                instead of using append, initialize the array to the size wanted using np.zeros(size) first
+                This is much faster
+                then add elements using numpy indexing new_data[i,:] = time series
+                """
             return new_data
 
 if __name__ == '__main__':
--- sampling.py
+++ sampling.py
@@ -7,14 +7,40 @@ Assuming that tmin is multiple of t or vice versa
 import numpy as np
 
 class Resampling:
+	"""
+	It is good practice to use the triple quotes and a few sentences about what the object or function does.
+	Inputs: data - description
+			t - description
+			tnew - description
+
+	Outputs: downsampled - description
+	"""
+
 	def __init__(self, data, t, tnew):
 		self.data = data
 		self.t = t
 		self.tnew = tnew
 	def downsampling(self, data, t, tnew):
+		"""
+		use numpy array instead of python list
+		downsampled = np.zeros(size of new array)
+		"""
 		downsampled = []
 		t_ratio = tnew // t
 		node_num = 0
+		"""
+		try using numpy array indexing
+		look up numpy array indexing on google to learn about it
+		It might be possible to do this in one line
+		downsampled = data[:,0:-1:t_ratio] This may not be correct. It is your job to check this.
+		Main Concept: numpy indexing has [] after variable with first : representing all the rows then
+					after the , is the columns. For more than 2 dimensionnal array each , separates the next dimension.
+					In the columns, the 0 means first element : separates and -1 is the last element. : separates and the
+					the final number is the number of spaces between the first and last that are sampled
+					It is possible to simplify 0:-1:t to ::t because the entire column is implied by just typing : as is done for rows
+		Check all of this with a google search for numpy indexing
+		"""  
+
 		for node in data:
 			i = 0
 			time_sum = 0
@@ -31,9 +57,23 @@ class Resampling:
 		return downsampled
 
 	def upsampling(self, data, t, tnew):
+		"""
+		use numpy array instead of python list
+		upsampled = np.zeros(size of new array)
+		"""
 		upsampled = []
 		upsampled_final = []
 		node_num = 0
+		"""
+		you can use . to make things into a float
+		highly recommended for division
+		min_per_day = 1440.
+		to make whole numpy arrays into floats
+		data = np.array(data,dtype=float)
+		It is good practice to put things like this into the begining of your function to make them into the proper data type
+		This way you dont have to assume the user input the correct data type for your function to work
+		"""
+
 		min_per_day = 1440
 		t_ratio = t // tnew
 		pts_per_day = min_per_day // t
@@ -47,6 +87,13 @@ class Resampling:
 				upsampled[node_num].append([tot_at_time_of_day / days])
 			node_num += 1
 		node_num = 0
+		"""
+		Sigma should be calculated from the data not hardcoded.
+		google search numpy standard deviation to get the value
+		It will require all of the data, so do not sum it up like you do above
+		Just organize it into an array of shape 24 X number of days and use numpy mean and standard deviation on it
+		use np.zeros((24,days)) to initialize the shape first
+		"""
 		sigma = 0.1
 		for node in upsampled:
 			for time in node:
@@ -57,9 +104,12 @@ class Resampling:
 
 if __name__ == '__main__':
 	t = 5
-	tnew = 10
+	tnew = 1
 	data = np.genfromtxt('res_avg_load_data_5min_2days_no_headers.csv', delimiter=',')
 	sample = Resampling(data, t, tnew)
+	"""
+	combine the 2 functions into one function and put this if statement in the beginning
+	"""
 	if t > tnew:
 		ret = sample.upsampling(data, t, tnew)
 	else:
